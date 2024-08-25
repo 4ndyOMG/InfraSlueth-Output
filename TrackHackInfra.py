ff69409b6c0215be2a42f302219393ca5b75a48b
4ndyOMG <54820803+4ndyOMG@users.noreply.github.com>
2024-08-25 10:34:52 +0000

    commit from within python file using !command

--- TrackHackInfra.py
+++ TrackHackInfra.py
@@ -1,8 +1,9 @@
 import os
 
-f = open("demofile3.txt", "a")
+f = open("demofile4.txt", "a")
 f.write("Now the file has more content!")
 f.close()
 
 os.system('git add .')
 os.system('git commit -m "commit from within python file using !command"')
+os.system('git push origin main')
