ac2b4780ee191546d4c31a0becdc3aa3232a572f
4ndyOMG <54820803+4ndyOMG@users.noreply.github.com>
2024-08-25 10:39:15 +0000

    changes from bash to check python

--- TrackHackInfra.py
+++ TrackHackInfra.py
@@ -1,9 +1,14 @@
 import os
 
-f = open("demofile4.txt", "a")
+# Get latest Python script from repo.
+os.system('git pull origin main')
+
+# Create a file
+f = open("demofile6.txt", "a")
 f.write("Now the file has more content!")
 f.close()
 
+# Push output of script to repo. 
 os.system('git add .')
 os.system('git commit -m "commit from within python file using !command"')
 os.system('git push origin main')
