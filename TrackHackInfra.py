eb9674f005d36e860ab607d4680db2ab135d16df
4ndyOMG <54820803+4ndyOMG@users.noreply.github.com>
2024-08-25 10:50:37 +0000

    commit to public repo

--- TrackHackInfra.py
+++ TrackHackInfra.py
@@ -1,14 +1,28 @@
 import os
 
 # Get latest Python script from repo.
+os.system('git remote add origin https://github.com/4ndyOMG/TrackHackInfra.git')
 os.system('git pull origin main')
 
 # Create a file
-f = open("demofile6.txt", "a")
-f.write("Now the file has less content!")
+f = open("demofile1.txt", "a")
+f.write("IOCs to follow.")
 f.close()
 
-# Push output of script to repo. 
+# Push output of script to CURRENT repo branch.
+os.system('git remote add origin https://github.com/4ndyOMG/TrackHackInfra-Output.git')
 os.system('git add .')
-os.system('git commit -m "commit from within python file using !command"')
+os.system('git commit -m "commit to public repo"')
 os.system('git push origin main')
+
+
+# Notes from google
+# change branch and commit only output
+
+#You have to create the branch as usual:
+#git checkout -b TheBranchToUse
+#Afterwards you need to make it public to others:
+#git push -u origin TheBranchToUse
+
+# add new remote repo
+#os.system('git remote add origin https://github.com/4ndyOMG/TrackHackInfra-Output.git)'
